a0acbea8dabda904dab523aacc1b0188536f68ab
StjWorld <st_murat@hotmail.com>
2023-02-03 14:22:03 -0600

    first commit

---
+++ Inventorymicroservice/src/test/java/com/mtumer/InventorymicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorymicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
